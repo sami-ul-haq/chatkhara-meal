b2f815063def607686e1f6939e29dbf91f9637b1
Sami ul Haq <samiulhaq1899@gmail.com>
2020-09-04 23:30:55 +0500

    initial

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+// Script For Slider
+$(document).ready(function () {
+    $('.my-slider').slick({
+        autoplay:true,
+        slidesToShow:1,
+        slidesToScroll:1,
+        prevArrow:'.prev-btn',
+        nextArrow:'.next-btn',
+        speed:1000,
+    });
+});
